ea3222e8a7e4257f2fee1a1d2b5cd9998738924d
Eugene Ha <eha@posteo.de>
2017-07-18 09:27:43 +0200

    Basic unit tests

---
+++ tests/testthat/test-eff.R
@@ -0,0 +1,26 @@
+context("eff")
+
+test_that("eff() can make unary functions without default values", {
+  expect_equal(eff(x ~ x + 1), function(x) x + 1)
+})
+
+test_that("eff() can make unary functions with default values", {
+  expect_equal(eff(x = 1 ~ x + 1), function(x = 1) x + 1)
+})
+
+test_that("eff() can make variadic functions", {
+  expect_equal(eff(... ~ list(...)), function(...) list(...))
+})
+
+test_that("eff() can make multi-argument functions without default values", {
+  expect_equal(eff(x, y ~ x + y), function(x, y) x + y)
+})
+
+test_that("eff() can make multi-argument functions with default values", {
+  expect_equal(eff(x, y = 1 ~ x + y), function(x, y = 1) x + y)
+})
+
+test_that("eff() can make multi-argument, variadic functions", {
+  expect_equal(eff(x, y = 1, ... ~ x + y), function(x, y = 1, ...) x + y)
+  expect_equal(eff(x, ... = , y = 1 ~ x + y), function(x, ..., y = 1) x + y)
+})
