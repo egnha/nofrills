eb0a07542589f731e1f1edee0696fc6b04ad09a9
Eugene Ha <eha@posteo.de>
2017-09-14 18:35:04 +0200

    Why exprs_()

--- R/exprs.R
+++ R/exprs.R
@@ -1,3 +1,12 @@
+#' Raw quotation of expressions
+#'
+#' `exprs_()` is an extension of [rlang::exprs()] that comprehends literal
+#' unquoting operators: `QUQ()`, `QUQS()`, `QUQE()`.
+#'
+#' @param ...,.ignore_empty Same as for [rlang::exprs()].
+#' @return List of expressions.
+#'
+#' @noRd
 exprs_ <- function(..., .ignore_empty = "trailing") {
   lapply(quos(..., .ignore_empty = .ignore_empty), f_rhs_)
 }
