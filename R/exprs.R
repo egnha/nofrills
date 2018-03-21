cab0ed21326b5da145cb86e4e12f7afd1521ec1e
Eugene Ha <eha@posteo.de>
2018-03-21 16:03:43 +0100

    Eliminate dependence on implementation detail of quosures
    Closes #11

--- R/exprs.R
+++ R/exprs.R
@@ -3,15 +3,15 @@
 #' `exprs_()` is an extension of [rlang::exprs()] that comprehends literal
 #' unquoting operators: `QUQ()`, `QUQS()`, corresponding to `!!` and `!!!` resp.
 #'
-#' @param ...,.ignore_empty Same as for [rlang::exprs()].
+#' @param ... Expressions to capture, unevaluated.
 #' @return List of expressions.
 #'
 #' @noRd
-exprs_ <- local({
-  quote_uq <- list(QUQ = as.name("!!"), QUQS = as.name("!!!"))
-  f_rhs_ <- function(x)
-    do.call("substitute", list(f_rhs(x), quote_uq))
+exprs_ <- function(...)
+  lapply(quos(...), quo_get_expr_)
 
-  function(..., .ignore_empty = "trailing")
-    lapply(quos(..., .ignore_empty = .ignore_empty), f_rhs_)
+quo_get_expr_ <- local({
+  quote_uq <- list(QUQ = as.name("!!"), QUQS = as.name("!!!"))
+  function(x)
+    do.call("substitute", list(quo_get_expr(x), quote_uq))
 })
