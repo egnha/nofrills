788e21755e7f2499bddcfdc841b38b1d934f12dc
Eugene Ha <eha@posteo.de>
2018-04-01 13:56:38 +0200

    Cheaper predicate

--- R/curry.R
+++ R/curry.R
@@ -96,7 +96,7 @@ is_curried_ <- function(f) {
   fmls <- formals(f)
   length(fmls) <= 1 ||
     all_have_values(fmls) ||
-    c("__uncurry__", "__partialize__") %are% names(environment(f))
+    "__uncurry__" %in% names(environment(f))
 }
 
 all_have_values <- function(fmls)
