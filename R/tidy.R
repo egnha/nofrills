9e75bd976ff0679239433e3919c16e44c5b98e6f
Eugene Ha <eha@posteo.de>
2018-04-06 10:50:43 +0200

    Make a function tidy

---
+++ R/tidy.R
@@ -0,0 +1,97 @@
+#' Make a function tidy
+#'
+#' @param f Function.
+#' @return Tidy function.
+#' @examples
+#' f <- function(x, y, ..., z = "z") list(x, y, z, ...)
+#' f_tidy <- tidy(f)
+#'
+#' stopifnot(
+#'   # Make ordinary calls as usual
+#'   identical(
+#'     f_tidy("x", "y", "w"),
+#'     f("x", "y", "w")
+#'   ),
+#'   # Splice arguments using ‘!!!’
+#'   identical(
+#'     f_tidy(y = "y", !!!list("x", a = "a", "b", c = "c")),
+#'     f(y = "y", "x", a = "a", "b", c = "c")
+#'   ),
+#'   # tidy() is the identity for tidy functions
+#'   identical(
+#'     f_tidy, tidy(f_tidy)
+#'   )
+#' )
+#'
+#' x <- local({
+#'   val <- "local value"
+#'   rlang::quo(val)
+#' })
+#' stopifnot(
+#'   # Unquote values using ‘!!’ to evaluate them immediately
+#'   identical(
+#'     f_tidy(!!x, "y"),
+#'     f("local value", "y")
+#'   )
+#' )
+#' @export
+tidy <- function(f) {
+  is.function(f) %because% "Only functions can be tidied"
+  if (is_tidy_(f))
+    return(f)
+  `__untidy_fun__` <- f
+  `__quo__` <- quo
+  f_tidy <- function() {
+    call <- `[[<-`(sys.call(), 1, `__untidy_fun__`)
+    call <- eval(as.call(c(`__quo__`, call)), parent.frame())
+    eval_tidy(call)
+  }
+  formals(f_tidy) <- formals(closure(f))
+  class(f_tidy) <- c("TidyFunction", class(f))
+  f_tidy
+}
+
+#' @rdname tidy
+#' @param x Object to test: Is it a tidy function?
+#' @examples
+#' stopifnot(
+#'   is_tidy(tidy(f)),
+#'   !is_tidy(f),
+#'   !is_tidy(untidy(tidy(f)))
+#' )
+#' @export
+is_tidy <- function(x) {
+  if (is.function(x))
+    return(is_tidy_(x))
+  FALSE
+}
+
+is_tidy_ <- function(x) {
+  env <- environment(x)
+  if (is.null(env))
+    return(FALSE)  # Primitive functions are never tidy
+  exists("__untidy_fun__", envir = env, mode = "function", inherits = FALSE)
+}
+
+#' @rdname tidy
+#' @examples
+#' stopifnot(
+#'   # untidy() recovers the “untidy” function underlying a tidy function
+#'   identical(f, untidy(tidy(f))),
+#'   identical(f, untidy(f))
+#' )
+#' @export
+untidy <- function(f) {
+  is.function(f) %because% "Only functions can be untidied"
+  untidy_(f) %||% f
+}
+
+untidy_ <- getter("__untidy_fun__", environment)
+
+#' @export
+print.TidyFunction <- function(x, ...) {
+  cat("<Tidy Function>\n\n")
+  print.default(untidy_(x))
+  cat("\n(Apply 'untidy()' to recover the function shown)")
+  invisible(x)
+}
