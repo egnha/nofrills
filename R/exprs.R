55b3f85fb6af9e20b60d7c1bad7cc947d669dc44
Eugene Ha <eha@posteo.de>
2018-04-25 15:04:41 +0200

    Tidy code

--- R/exprs.R
+++ R/exprs.R
@@ -1,18 +1,24 @@
 #' Raw quotation of expressions
 #'
 #' `exprs_()` is an extension of [rlang::exprs()] that comprehends literal
-#' unquoting operators: `QUQ()`, `QUQS()`, corresponding to `!!` and `!!!` resp.
+#' unquoting operators: `QUQ()`, `QUQS()` are substituted as `` `!!`() ``,
+#' `` `!!!`() ``, resp.
 #'
-#' @param ... Expressions to capture, unevaluated.
+#' @param ... Unevaluated expressions to capture.
 #' @return List of expressions.
 #'
 #' @noRd
-exprs_ <- function(...)
+exprs_ <- function(...) {
   lapply(quos(...), quo_get_expr_)
+}
 
 quo_get_expr_ <- local({
-  quote_uq <- list(QUQ = as.name("!!"), QUQS = as.name("!!!"))
+  quq <- list(
+    QUQ  = as.name("!!"),
+    QUQS = as.name("!!!")
+  )
 
-  function(x)
-    do.call("substitute", list(quo_get_expr(x), quote_uq))
+  function(x) {
+    do.call("substitute", list(quo_get_expr(x), quq))
+  }
 })
