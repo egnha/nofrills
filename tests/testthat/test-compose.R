fccf2aca33ab062e8b72dd9f2c5a68f8452efaba
Eugene Ha <eha@posteo.de>
2018-05-01 10:59:21 +0200

    Simplify

--- tests/testthat/test-compose.R
+++ tests/testthat/test-compose.R
@@ -123,19 +123,19 @@ test_that("nested compositions are flattened", {
   cmps <- list(
     compose(gs[[1]], gs[[2]], gs[[3]], gs[[4]]),
     compose(compose(gs[[1]], gs[[2]], gs[[3]], gs[[4]])),
-    compose((!!gs[[1]]) %<<<% (!!gs[[2]]) %<<<% (!!gs[[3]]) %<<<% (!!gs[[4]])),
-    compose((!!gs[[4]]) %>>>% (!!gs[[3]]) %>>>% (!!gs[[2]]) %>>>% (!!gs[[1]])),
+    compose((!!gs[[1]]) %<<<% !!gs[[2]] %<<<% !!gs[[3]] %<<<% !!gs[[4]]),
+    compose((!!gs[[4]]) %>>>% !!gs[[3]] %>>>% !!gs[[2]] %>>>% !!gs[[1]]),
     compose(gs[[1]], compose(gs[[2]], gs[[3]], gs[[4]])),
-    compose((!!gs[[1]]) %<<<% (!!compose(gs[[2]], gs[[3]], gs[[4]]))),
-    compose((!!compose(gs[[2]], gs[[3]], gs[[4]])) %>>>% (!!gs[[1]])),
-    compose(gs[[1]], compose(gs[[2]], (!!gs[[3]]) %<<<% (!!gs[[4]]))),
-    compose(gs[[1]], compose(gs[[2]], (!!gs[[4]]) %>>>% (!!gs[[3]]))),
+    compose((!!gs[[1]]) %<<<% !!compose(gs[[2]], gs[[3]], gs[[4]])),
+    compose((!!compose(gs[[2]], gs[[3]], gs[[4]])) %>>>% !!gs[[1]]),
+    compose(gs[[1]], compose(gs[[2]], (!!gs[[3]]) %<<<% !!gs[[4]])),
+    compose(gs[[1]], compose(gs[[2]], (!!gs[[4]]) %>>>% !!gs[[3]])),
     compose(gs[[1]], compose(gs[[2]], compose(gs[[3]], gs[[4]]))),
-    compose(gs[[1]], (!!gs[[2]]) %<<<% (!!compose(gs[[3]], gs[[4]]))),
-    compose(gs[[1]], (!!compose(gs[[3]], gs[[4]])) %>>>% (!!gs[[2]])),
+    compose(gs[[1]], (!!gs[[2]]) %<<<% !!compose(gs[[3]], gs[[4]])),
+    compose(gs[[1]], (!!compose(gs[[3]], gs[[4]])) %>>>% !!gs[[2]]),
     compose(gs[[1]], compose(gs[[2]], compose(gs[[3]], compose(gs[[4]])))),
-    compose((!!gs[[1]]) %<<<% !!((!!gs[[2]]) %<<<% !!((!!gs[[3]]) %<<<% (!!gs[[4]])))),
-    compose((!!((!!((!!gs[[4]]) %>>>% (!!gs[[3]]))) %>>>% (!!gs[[2]]))) %>>>% (!!gs[[1]]))
+    compose((!!gs[[1]]) %<<<% !!((!!gs[[2]]) %<<<% !!((!!gs[[3]]) %<<<% !!gs[[4]]))),
+    compose((!!((!!((!!gs[[4]]) %>>>% !!gs[[3]])) %>>>% (!!gs[[2]]))) %>>>% !!gs[[1]])
   )
   for (cmp in cmps)
     expect_equivalent(decompose(cmp), gs)
